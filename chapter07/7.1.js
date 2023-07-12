7390935fd0c8ab3e17a2dd7b049fa7736a09c881
Jay <jay@madsq.net>
2023-07-12 23:58:12 +0900

    feat: Chapter07

---
+++ chapter07/7.1.js
@@ -0,0 +1,20 @@
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+const organization = new Organization({ name: "이름" });
+// const organization = { name: '이름' };
+function getOrganization() {
+  return organization;
+}
+
+console.log(getOrganization().name);
+
+// getOrganization()._name = '이름2';
+
+// console.log(organization._name);
